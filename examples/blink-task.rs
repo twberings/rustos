1e7b5dabaf18e0ac15ec6e3140603ba827853fd3
Thijs Berings <twberings@gmail.com>
2026-03-22 16:42:36 +0100

    changed scheduler to a singleton

--- examples/blink-task.rs
+++ examples/blink-task.rs
@@ -7,18 +7,11 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use esp_hal::main;
-use esp_hal::{clock::CpuClock, delay::Delay};
-
 use defmt::info;
-
-use esp_hal::rmt::Rmt;
-use esp_hal::time::Rate;
-use esp_hal_smartled::{SmartLedsAdapter, smart_led_buffer};
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
 use panic_rtt_target as _;
 use rustos::prelude::*;
-use smart_leds::hsv::{Hsv, hsv2rgb};
-use smart_leds::{RGB8, SmartLedsWrite as _, brightness, gamma};
 
 extern crate alloc;
 
@@ -33,41 +26,15 @@ fn main() -> ! {
     rtt_target::rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
+    let _peripherals = esp_hal::init(config);
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 65536);
 
-    let mut _scheduler = Scheduler::new();
-
-    let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
-    let mut rmt_buffer = smart_led_buffer!(1);
-    let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, &mut rmt_buffer);
-    let delay = Delay::new();
-
-    let mut color = Hsv {
-        hue: 0,
-        sat: 255,
-        val: 255,
-    };
-    let mut data: RGB8;
-    let level = 20;
-
-    loop {
-        for hue in 0..=255 {
-            color.hue = hue;
-            data = hsv2rgb(color);
-            led.write(brightness(gamma([data].into_iter()), level))
-                .unwrap();
-            delay.delay_millis(1);
-        }
-    }
-
-    // scheduler
-    //     .spawn_task(1, blink_task)
-    //     .expect("Failed to spawn blink task");
-    // scheduler.run();
+    Scheduler::init();
+    Scheduler::spawn_task(1, blink_task).expect("Failed to spawn blink task");
+    Scheduler::run();
 }
 
-fn _blink_task(task: &mut Task) {
+fn blink_task(task: &mut Task) {
     info!("Blink!");
     task.delay(1000);
 }
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,3 +1,6 @@
+use core::cell::RefCell;
+use critical_section::Mutex;
+
 use crate::task::*;
 const MAX_TASKS: usize = 10;
 
@@ -6,36 +9,43 @@ pub struct Scheduler {
     tasks: [Option<Task>; MAX_TASKS],
 }
 
+static SCHEDULER: Mutex<RefCell<Option<Scheduler>>> = Mutex::new(RefCell::new(None));
+
 impl Scheduler {
-    pub fn new() -> Self {
-        Scheduler {
-            tasks: [None; MAX_TASKS],
-        }
+    pub fn init() {
+        critical_section::with(|cs| {
+            *SCHEDULER.borrow(cs).borrow_mut() = Some(Scheduler::default());
+        });
     }
 
-    pub fn spawn_task(
-        &mut self,
-        priority: usize,
-        entry: fn(&mut Task),
-    ) -> Result<&Task, SchedulerError> {
-        for slot in self.tasks.iter_mut() {
-            if slot.is_none() {
-                *slot = Some(Task::new(priority, entry));
-                return Ok(slot.as_ref().unwrap());
-            }
-        }
-        Err(SchedulerError::NoAvailableSlot)
-    }
-
-    pub fn run(&mut self) -> ! {
-        loop {
-            for task in self.tasks.iter_mut().flatten() {
-                match task.state {
-                    TaskState::Ready => task.run(),
-                    TaskState::Running => continue,
-                    _ => continue,
+    pub fn spawn_task(priority: usize, entry: fn(&mut Task)) -> Result<(), SchedulerError> {
+        critical_section::with(|cs| {
+            if let Some(ref mut scheduler) = *SCHEDULER.borrow(cs).borrow_mut() {
+                for slot in scheduler.tasks.iter_mut() {
+                    if slot.is_none() {
+                        *slot = Some(Task::new(priority, entry));
+                        return Ok(());
+                    }
                 }
+                return Err(SchedulerError::NoAvailableSlot);
             }
+            Err(SchedulerError::SchedulerNotInitialized)
+        })
+    }
+
+    pub fn run() -> ! {
+        loop {
+            critical_section::with(|cs| {
+                if let Some(ref mut scheduler) = *SCHEDULER.borrow(cs).borrow_mut() {
+                    for task in scheduler.tasks.iter_mut().flatten() {
+                        match task.state {
+                            TaskState::Ready => task.run(),
+                            TaskState::Running => continue,
+                            _ => continue,
+                        }
+                    }
+                }
+            })
         }
     }
 }
@@ -43,4 +53,5 @@ impl Scheduler {
 #[derive(Debug)]
 pub enum SchedulerError {
     NoAvailableSlot,
+    SchedulerNotInitialized,
 }
